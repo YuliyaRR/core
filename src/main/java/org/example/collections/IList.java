7b76f6b1da3a2773ca7be8e4f63dcb7eee4d9f91
Yuliya Romashko <ramashkay@gmail.com>
2023-05-16 16:33:40 +0300

    Added: method contains() to IList, changed return type for methods add(): void->boolean

--- src/main/java/org/example/collections/IList.java
+++ src/main/java/org/example/collections/IList.java
@@ -2,13 +2,14 @@ package org.example.collections;
 
 import java.util.Comparator;
 
-public interface IList <E> {
-    void add(E element);
-    void add(int index, E element);
+public interface IList <E> extends ICollection<E> {
+    boolean add(E element);
+    boolean add(int index, E element);
     E get (int index);
     boolean remove(int index);
     boolean remove(E element);
     int size();
     void clear();
     void sort(Comparator<? super E> comparator);
+    boolean contains(E element);
 }
