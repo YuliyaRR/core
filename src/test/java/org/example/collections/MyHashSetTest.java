b607b2f6289ed3f3718a42e2bb01de777e83d8f6
Yuliya Romashko <ramashkay@gmail.com>
2023-08-16 22:01:48 +0300

    Added: @BeforeEach annotation in MyHashSetTest

--- src/test/java/org/example/collections/MyHashSetTest.java
+++ src/test/java/org/example/collections/MyHashSetTest.java
@@ -1,50 +1,52 @@
 package org.example.collections;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MyHashSetTest {
+    private ISet<Integer> hashSet;
+    @BeforeEach
+    public void setUp() {
+        this.hashSet = new MyHashSet<>();
+    }
+
     @Test
     public void addSingleElementIsTrue(){
-        ISet<Integer> hashSet = new MyHashSet<>();
-        Assertions.assertTrue(hashSet.add(15));
+       assertTrue(hashSet.add(15));
     }
 
     @Test
     public void addDuplicateElementIsFalse(){
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(15);
-        Assertions.assertFalse(hashSet.add(15));
+        assertFalse(hashSet.add(15));
     }
 
     @Test
     public void addElementWithTheSameIndexCheckRecursionFirstLoopIsTrue(){
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(15);
-        Assertions.assertTrue(hashSet.add(31));
+        assertTrue(hashSet.add(31));
     }
 
     @Test
     public void addElementWithTheSameIndexCheckRecursionSecondLoopIsTrue(){
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(15);
         hashSet.add(31);
-        Assertions.assertTrue(hashSet.add(47));
+        assertTrue(hashSet.add(47));
     }
 
     @Test
     public void addDuplicateElementWithTheSameIndexCheckRecursionSecondLoopIsTrue(){
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(15);
         hashSet.add(31);
-        Assertions.assertFalse(hashSet.add(31));
+        assertFalse(hashSet.add(31));
     }
 
     @Test
-    public void addManyElementsCheckIncreaseInternalArray(){
-        ISet<Integer> hashSet = new MyHashSet<>();
+    public void addManyElementsCheckIncreaseInternalArray(){//?
         hashSet.add(1);
         hashSet.add(2);
         hashSet.add(3);
@@ -57,12 +59,11 @@ public class MyHashSetTest {
         hashSet.add(10);
         hashSet.add(11);
         hashSet.add(12);
-        Assertions.assertTrue(hashSet.add(31));
+        assertTrue(hashSet.add(31));
     }
 
     @Test
     public void addManyElementsCheckRestructuringOfInternalArray(){
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(1);
         hashSet.add(2);
         hashSet.add(3);
@@ -78,155 +79,141 @@ public class MyHashSetTest {
         hashSet.add(11);
         hashSet.add(12);
 
-        Assertions.assertTrue(hashSet.add(51));
+        assertTrue(hashSet.add(51));
     }
 
     @Test
     public void removeElementFromCellIsTrue() {
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(1);
         hashSet.add(2);
         hashSet.add(3);
 
-        Assertions.assertTrue(hashSet.remove(2));
+        assertTrue(hashSet.remove(2));
     }
 
     @Test
     public void removeElementFromCellCheckSize() {
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(1);
         hashSet.add(2);
         hashSet.add(3);
 
         hashSet.remove(2);
 
-        Assertions.assertEquals(2, hashSet.size());
+        assertEquals(2, hashSet.size());
     }
 
     @Test
     public void removeNonExistElementFromCellIsFalse() {
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(1);
         hashSet.add(2);
         hashSet.add(3);
 
-        Assertions.assertFalse(hashSet.remove(6));
+        assertFalse(hashSet.remove(6));
     }
 
     @Test
     public void removeElementFromCellNextIsNotNull() {
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(15);
         hashSet.add(31);
 
         hashSet.remove(15);
 
-        Assertions.assertEquals(1, hashSet.size());
+        assertEquals(1, hashSet.size());
     }
 
     @Test
     public void removeElementFromInternalLinkedListNextIsNull() {
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(15);
         hashSet.add(31);
 
-        Assertions.assertTrue(hashSet.remove(31));
+        assertTrue(hashSet.remove(31));
     }
 
     @Test
     public void removeElementFromInternalLinkedListCheckSize() {
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(15);
         hashSet.add(31);
         hashSet.remove(31);
 
-        Assertions.assertEquals(1, hashSet.size());
+        assertEquals(1, hashSet.size());
     }
 
     @Test
     public void removeElementFromInternalLinkedListIs2() {
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(15);
         hashSet.add(31);
         hashSet.add(47);
         hashSet.remove(31);
 
-        Assertions.assertEquals(2, hashSet.size());
+        assertEquals(2, hashSet.size());
     }
 
     @Test
     public void removeElementFromInternalLinkedListIs3() {
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(15);
         hashSet.add(31);
         hashSet.add(47);
         hashSet.remove(47);
 
-        Assertions.assertEquals(2, hashSet.size());
+        assertEquals(2, hashSet.size());
     }
 
     @Test
     public void cleanCheckSize() {
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(1);
         hashSet.add(2);
         hashSet.add(3);
 
         hashSet.clear();
 
-        Assertions.assertEquals(0, hashSet.size());
+        assertEquals(0, hashSet.size());
     }
 
     @Test
     public void containsIsTrue() {
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(1);
         hashSet.add(2);
         hashSet.add(3);
 
-        Assertions.assertTrue(hashSet.contains(2));
+        assertTrue(hashSet.contains(2));
     }
 
     @Test
     public void containsIsFalse() {
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(1);
         hashSet.add(2);
         hashSet.add(3);
 
-        Assertions.assertFalse(hashSet.contains(12));
+        assertFalse(hashSet.contains(12));
     }
 
     @Test
     public void containsIsTrueInternalLinkedList() {
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(15);
         hashSet.add(31);
         hashSet.add(47);
 
-        Assertions.assertTrue(hashSet.contains(47));
+        assertTrue(hashSet.contains(47));
     }
 
     @Test
     public void containsIsFalseInternalLinkedList() {
-        ISet<Integer> hashSet = new MyHashSet<>();
         hashSet.add(15);
         hashSet.add(31);
 
-        Assertions.assertFalse(hashSet.contains(47));
+        assertFalse(hashSet.contains(47));
     }
 
     @Test
     public void forEachTest() {
-        ISet<Integer> set = new MyHashSet<>();
         for (int i = 0; i < 10; i++) {
-            set.add(ThreadLocalRandom.current().nextInt(100));
+            hashSet.add(ThreadLocalRandom.current().nextInt(100));
         }
         int index = 0;
-        for (Integer num: set) {
+        for (Integer num: hashSet) {
             index++;
         }
-        Assertions.assertEquals(set.size(), index);
+        assertEquals(hashSet.size(), index);
     }
 
 }
